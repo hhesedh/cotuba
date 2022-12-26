64324e9a7685a30f1e2d58d80f392174cc852ee0
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2022-12-26 14:47:39 -0300

    refatorando projeto para utilizar o RenderizadorMDParaHTML

--- src/main/java/cotuba/Main.java
+++ src/main/java/cotuba/Main.java
@@ -1,6 +1,7 @@
 package cotuba;
 
 import java.nio.file.Path;
+import java.util.List;
 
 
 public class Main {
@@ -20,14 +21,21 @@ public class Main {
       arquivoDeSaida = opcoesCLI.getArquivoDeSaida();
       modoVerboso = opcoesCLI.isModoVerboso();
 
+        var renderizadorMDParaHTML = new RenderizadorMDParaHTML();
+      List<Capitulo> capitulos = renderizadorMDParaHTML.renderiza(diretorioDosMD);
+      var ebook = new Ebook();
+      ebook.setFormato(formato);
+      ebook.setArquivoDeSaida(arquivoDeSaida);
+      ebook.setCapitulos(capitulos);
+
       if ("pdf".equals(formato)) {
 
           var geradorPDF = new GeradorPDF();
-          geradorPDF.gera(diretorioDosMD, arquivoDeSaida);
+          geradorPDF.gera(ebook);
 
       } else if ("epub".equals(formato)) {
           var geradorEPUB = new GeradorEPUB();
-          geradorEPUB.gera(diretorioDosMD, arquivoDeSaida);
+          geradorEPUB.gera(ebook);
       } else {
         throw new IllegalArgumentException("Formato do ebook inválido: " + formato);
       }
